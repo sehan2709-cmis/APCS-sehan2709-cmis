24dd2fb9db9aad0766c753b7bec18995e47223ca
Sehan Kwon <sehan@cmis.ac.th>
2018-08-09 14:30:20 +0700

    Finished FirstPorject

---
+++ FirstProject/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+    public static void main ( String args [] )
+    {
+        System.out.println( "Hello everyone out there! ");
+    } // ebd netgid.main 
+} // end class Helloworld
