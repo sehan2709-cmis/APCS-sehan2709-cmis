22bc31a6e1e3729b3f38f047a82bb76f3cf1e831
Your Name <you@example.com>
2018-08-14 09:33:31 +0700

    Short Description

---
+++ FirstProject/Fundamentalist.java
@@ -0,0 +1,24 @@
+
+public class Fundamentalist
+{
+    public static void main(String args[]){
+        dataTypes();
+    }
+
+        public static void dataTypes(){
+            byte a = -128;
+            byte b = 127;
+            short c = -32768;
+            short d = 32767;
+            int e = -2147483648;
+            int f = 2147483647;
+            long g = -9223372036854775808L;
+            long h = 9223372036854775807L;
+            
+            //Code the problem here.
+        }
+        
+        public static void add(int a, int b){
+        
+    }
+}
