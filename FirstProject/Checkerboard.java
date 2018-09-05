f30176c439861071c95b45b607bc2b408215c7ed
sehan2709-cmis <sehan2709@cmis.ac.th>
2018-09-05 09:55:32 +0700

    I might need just one da to finish Fundamentals I

--- FirstProject/Checkerboard.java
+++ FirstProject/Checkerboard.java
@@ -3,22 +3,43 @@ import java.util.Random;
 
 public class Checkerboard
 {
-    public static void cherckerboard(int hight, int width){
+    public static void cherckerboard(int height, int width){
         String sharp = "";
         String border = "+";
         String work = "|";
+        int r = (int)(Math.random()*(10));
         for (int w = width; w > 0; w--){
             border += "-";
         }
         border += "+";
         System.out.println(border);
-        for (int h = hight; h > 0; h--){
-            for (int w = width; w > 0; w--){
-                System.out.print(".");
-                if (w == 0){
-                    System.out.print("|\n");
+        for (int h = 0; h < height; h++){
+            System.out.print("|");
+            for (int w = 0; w < width; w++){
+                if (w % 2 == 0){
+                    if (h % 2 ==0){
+                        System.out.print("#");
+                    }
+                    else if (h % 2 ==1){
+                        System.out.print(" ");
+                    }
+                }
+                else if (w % 2 == 1){
+                    if (h % 2 ==0){
+                        System.out.print(" ");
+                    }
+                    else if (h % 2 ==1){
+                        System.out.print("#");
+                    }
+                
+                }
+                if (w == width-1){
+                    String right = "|\n";
+                    System.out.print(right.trim());
+                    System.out.print("\n");
                 }
             }
         }
+        System.out.println(border);
     }
 }
