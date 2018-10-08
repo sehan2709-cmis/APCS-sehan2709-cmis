98e0d37b61abcee8863a4c351465f0bc674e70ca
sehan2709-cmis <sehan2709@cmis.ac.th>
2018-10-08 11:54:30 +0700

    f

--- FirstProject/Fundamentals_II/MakeGroups.java
+++ FirstProject/Fundamentals_II/MakeGroups.java
@@ -34,7 +34,7 @@ public class MakeGroups
         }
         return array;
     }
-    
+
     public static String[] makeGroup(String[] names, int groupSize)
     {
         int l = names.length/groupSize;
@@ -44,12 +44,48 @@ public class MakeGroups
         }
         String[] array = new String[l];
         int x =0;
+        int y = 0;
         while (x<array.length)
         {
-            int ranom = (int)((Math.random() * (names.length - 1)));
-            array[x] = names[ranom];
+            y = 0;
+            while( y<groupSize)
+            {
+                int ranom = (int)((Math.random() * (names.length - 1)));
+                array[y] = names[ranom];
+                y++;
+            }
             x++;
         }
         return array;
+    }
+
+    public static String[] shuffle(String[] names)
+    {
+        String[] array = new String[names.length];
+        int x = 0;
+        int[] ram = new int[names.length];
+        int z = 0;
+        int n = 0; 
+        while ( z < ram.length)
+        {   
+            n = 0;
+            int ranom = (int)((Math.random() * (names.length - 1)));
+            int ramon = (int)((Math.random() * (names.length - 1)));
+            if ( ram[ranom] == null )
+            ram[z] = ranom;
+            z++;
+        }
+        while ( x < array.length)
+        {
+            array[x] = names[ram[x]];
+            x++;
+        }
+        int y = 0;
+        while ( y < array.length)
+        {
+            names[y] = array[y];
+            y++;
+        }
+        return names;
     }
 }
