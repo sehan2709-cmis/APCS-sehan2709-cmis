69d580c6d8151333a5f8968e99bf94e3d98941f8
a <a>
2019-01-21 09:54:24 +0700

    a

---
+++ FirstProject/Sort/MergeSort.java
@@ -0,0 +1,19 @@
+package Sort;
+
+public class MergeSort
+{
+    public static int[] MergeSort(int[] array)
+    {
+        if( array.length == 1)
+        {
+            return array;
+        }
+        else if()
+        {
+        }
+        else
+        {
+            return array;
+        }
+    }
+}
