0ecbc68656fa6017e91ac93fd79eff0dd29c02aa
a <se>
2019-01-22 12:05:17 +0700

    a

--- FirstProject/Sort/MergeSort.java
+++ FirstProject/Sort/MergeSort.java
@@ -4,12 +4,28 @@ public class MergeSort
 {
     public static int[] MergeSort(int[] array)
     {
-        if( array.length == 1)
+        if( array.length != 1)
         {
-            return array;
-        }
-        else if()
-        {
+            int satta = array.length;
+            int[] x = new int [array.length/2];
+            int[] y = new int [array.length - x.length];
+            MergeSort(x);
+            MergeSort(y);
+            int[] z = new int [array.length];
+            int a = 0;
+            int b = 0;
+            for (int ang = 0; ang < array.length; ang++)
+            {
+                if(x[a] > y[b])
+                {
+                    
+                    a++;
+                }
+                else if(x[a] < y[b])
+                {
+                    b++;
+                }
+            }
         }
         else
         {
