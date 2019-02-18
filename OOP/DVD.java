5b93950046331f5d0715d3ee9b17407d1fe623e0
a <asdf>
2019-02-18 14:34:47 +0700

    a

--- OOP/DVD.java
+++ OOP/DVD.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DVD
 {
@@ -10,11 +11,37 @@ public class DVD
         System.out.println(list2.showing());
         Clock time1 = new Clock();
         System.out.println(time1.timeis());
+        
         int userh = Integer.parseInt(JOptionPane.showInputDialog("Type your Hour: "));
         int userm = Integer.parseInt(JOptionPane.showInputDialog("Type your Minutes: "));
-        int users = Integer.parseInt(JOptionPane.showInputDialog("Type your Seoncds: "));
+        int users = Integer.parseInt(JOptionPane.showInputDialog("Type your Seconds: "));
         Clock time2 = new Clock(userh, userm, users);
         System.out.println(time2.timeis());
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type your Hour: ");
+        int userhh = input.nextInt();
+        System.out.print("Type your Minutes: ");
+        int usermm = input.nextInt();
+        System.out.print("Type your Seconds: ");
+        int userss = input.nextInt();
+        Clock time3 = new Clock(userhh, usermm, userss);
+        System.out.println(time3.timeis());
+        
+        Students student1 = new Students();
+        System.out.println(list1.showing());
+        Students student2 = new Students("Youngil", "Kim", 12, false);
+        System.out.println(list2.showing());
+        Students nainai = new Students();
+        System.out.println(nainai.identity());
+        
+        String first = String.parseStr(JOptionPane.showInputDialog("Type your Hour: "));
+        String last = String.parseStr(JOptionPane.showInputDialog("Type your Minutes: "));
+        int gradee = Integer.parseInt(JOptionPane.showInputDialog("Type your Seconds: "));
+        boolean sexx = Integer.parseInt(JOptionPane.showInputDialog("Type your Seconds: "));        Clock time2 = new Clock(userh, userm, users);
+        System.out.println(time2.timeis());
+        
+        Students yeye = new Students(first, last, gradee, sexx);
+        System.out.println(yeye.identity());
     }
-
 }
